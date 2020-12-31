b971007301a52c6f25e958b630526898cf4dae18
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2020-12-31 17:16:40 -0500

    set up simple server to take connections

--- src/Server.ts
+++ src/Server.ts
@@ -1,1 +1,11 @@
-console.log("Hello World")
+import { Socket } from "dgram";
+import * as net from "net";
+
+let server = net.createServer((socket) => {
+    console.log(`${socket.remoteAddress} has been connected!`);
+})
+
+const PORT = 8080
+server.listen(PORT, () => {
+    console.log(`Running on port ${PORT}`)
+})
